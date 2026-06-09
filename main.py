dc3413d4d3fb9faccdad117ff4645aa79494ea6b
pearthureis <pearthur.reis@gmail.com>
2026-06-09 17:37:39 -0300

    painel inicial

---
+++ main.py
@@ -0,0 +1,22 @@
+from src.telemetria import gerar_telemetria
+from src.alertas import verificar_alertas
+
+dados = gerar_telemetria()
+
+print("===== CONNECTSAT AI =====")
+print("Status atual do satélite:\n")
+
+print(f"Latência: {dados['latencia_ms']} ms")
+print(f"Throughput: {dados['throughput_mbps']} Mbps")
+print(f"Temperatura: {dados['temperatura_transponder']} °C")
+print(f"Qualidade do sinal: {dados['qualidade_sinal']} %")
+
+print("\n===== ALERTAS =====")
+
+alertas = verificar_alertas(dados)
+
+if alertas:
+    for alerta in alertas:
+        print(alerta)
+else:
+    print("Nenhum problema detectado.")
